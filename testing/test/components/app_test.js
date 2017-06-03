91e3010569ffd5617b3d860719886c3cbe196d67
Peter Donald <peter@realityforge.org>
2017-06-03 14:13:39 +1000

    Add some documentation

--- testing/test/components/app_test.js
+++ testing/test/components/app_test.js
@@ -1,6 +1,7 @@
 import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
+// use 'describe' to group similar tests together
 describe('App' , () => {
   let component;
 
@@ -8,6 +9,7 @@ describe('App' , () => {
     component = renderComponent(App);
   });
 
+  // use 'it' to test a single attribute of a target
   it('renders something', () => {
     expect(component).to.exist;
   });
