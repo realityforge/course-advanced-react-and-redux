0f79cc9ce9e1398159ad6c8ebe5b7e6f00d10e4c
Peter Donald <peter@realityforge.org>
2017-06-09 08:12:30 +1000

    Start to introduce the application components

---
+++ hoc/src/components/resources.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default function () {
+  return (
+    <div>Resources</div>
+  );
+}
--- hoc/src/index.js
+++ hoc/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
+import { browserHistory, Route, Router } from 'react-router';
+import { applyMiddleware, createStore } from 'redux';
 import App from './components/app';
+import Resources from './components/resources';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()( createStore );
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+  <Provider store={createStoreWithMiddleware( reducers )}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <Route path="/resources" component={Resources}/>
+      </Route>
+    </Router>
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector( '.container' ) );
