653cc8625322014a32e1726d9067a31f9ffd4749
Peter Donald <peter@realityforge.org>
2017-08-13 10:33:43 +1000

    Add bcrypt-nodejs module and use it to encrpt password

--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt-nodejs');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -5,6 +6,31 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
+});
+
+// Hook into save and encrypt password before saving model
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  // generate a salt
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err);
+    } else if (err) {
+      return next(err);
+    } else {
+      // Hash the password using the salt
+      bcrypt.hash(user.password, salt, null, function(err, hash) {
+        if (err) {
+          return next(err);
+        } else {
+          // Replace password with encrypted variant
+          user.password = hash;
+          next();
+        }
+      });
+    }
+  });
 });
 
 // Create the model class
