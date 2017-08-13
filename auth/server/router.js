23b67f8f139c9f1704de85c3ecdaa5d9f621b33c
Peter Donald <peter@realityforge.org>
2017-08-13 11:39:34 +1000

    Require authentication for root url

--- auth/server/router.js
+++ auth/server/router.js
@@ -1,8 +1,12 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = function(app) {
   app.get('/signup', Authentication.signup);
-  app.get('/', function(req, res, next) {
+  app.get('/', requireAuth, function(req, res, next) {
     res.send(['ignore-me']);
   });
 };
