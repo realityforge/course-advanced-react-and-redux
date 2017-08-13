d7142d34a7896f178d1102c2735a6769ecab8f9f
Peter Donald <peter@realityforge.org>
2017-08-13 10:53:32 +1000

    Add some requires

---
+++ auth/server/services/passport.js
@@ -0,0 +1,5 @@
+const User = require('../models/user');
+const passport = require('passport');
+const passportJwt = require('passport-jwt');
+const JwtStrategy = passportJwt.Strategy;
+const ExtractJwt = passportJwt.ExtractJwt;
