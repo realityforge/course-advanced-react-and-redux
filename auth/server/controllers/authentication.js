0bca0b8459e29ab7014b8eb53929452287636e5e
Peter Donald <peter@realityforge.org>
2017-08-13 10:21:16 +1000

    Require email and password fields

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -2,27 +2,31 @@ import User from '../models/user';
 
 module.signup = function(req, resp, next) {
   const { email, password } = req.body;
+  if (!email || !email) {
+    //return an error if email or password not provided
+    return resp.status(422).send({ error: 'You must provide an email and password' });
+  } else {
+    //See if a user with email exists to avoid duplicates
+    User.findOne({ email }, function(err, user) {
+      if (err) {
+        return next(err);
+      } else if (user) {
+        //return an error if email exists
+        return resp.status(422);
+      } else {
+        //else create+save user record
+        const user = new User({ email, password });
 
-  //See if a user with email exists to avoid duplicates
-  User.findOne({ email }, function(err, user) {
-    if (err) {
-      return next(err);
-    } else if (user) {
-      //return an error if email exists
-      return resp.status(422);
-    } else {
-      //else create+save user record
-      const user = new User({ email, password });
+        user.save(function(err) {
+          if (err) {
+            return next(err);
 
-      user.save(function(err) {
-        if (err) {
-          return next(err);
-
-        } else {
-          //return success response with user details
-          resp.json({ success: true });
-        }
-      });
-    }
-  });
+          } else {
+            //return success response with user details
+            resp.json({ success: true });
+          }
+        });
+      }
+    });
+  }
 };
