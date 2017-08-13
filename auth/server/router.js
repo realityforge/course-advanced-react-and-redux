96d00a9c92f1b6338f8ea7ef7b03a3dd8e17e02c
Peter Donald <peter@realityforge.org>
2017-08-13 11:45:47 +1000

    Setup signin route

--- auth/server/router.js
+++ auth/server/router.js
@@ -3,8 +3,10 @@ const passportService = require('./services/passport');
 const passport = require('passport');
 
 const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
+  app.post('/signin', requireSignin, Authentication.signin);
   app.get('/signup', Authentication.signup);
   app.get('/', requireAuth, function(req, res, next) {
     res.send(['ignore-me']);
