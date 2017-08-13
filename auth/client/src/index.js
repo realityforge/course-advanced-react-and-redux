3c8b094b6d839f9a35889b534c81361f3eaf30cc
Peter Donald <peter@realityforge.org>
2017-08-13 21:48:15 +1000

    Externalize history

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import {applyMiddleware, createStore} from 'redux';
-
 import App from './components/app';
+import Signin from './components/auth/signin';
 import Header from './components/header';
+import history from './history';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter>
+    <BrowserRouter history={history}>
       <div>
         <Header/>
         <Switch>
