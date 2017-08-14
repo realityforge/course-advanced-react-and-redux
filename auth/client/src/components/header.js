f0919a3f153209ffadbd56a1e9cfe9a028411f3c
Peter Donald <peter@realityforge.org>
2017-08-14 22:37:36 +1000

    Render header based on authenticated status with required links

--- auth/client/src/components/header.js
+++ auth/client/src/components/header.js
@@ -1,13 +1,48 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
-export default class Header extends Component {
+class Header extends Component {
+  static renderSignOut() {
+    return (
+      <li className="nav-item">
+        <Link className="nav-link" to="/signout">Sign Out</Link>
+      </li>
+    );
+  }
+
+  static renderSignUp() {
+    return (
+      <li className="nav-item">
+        <Link className="nav-link" to="/signup">Sign Up</Link>
+      </li>
+    );
+  }
+
+  static renderSignIn() {
+    return (
+      <li className="nav-item">
+        <Link className="nav-link" to="/signin">Sign In</Link>
+      </li>
+    );
+  }
+
   render() {
     return (
       <nav className="navbar navbar-light">
+        <Link to="/" className="navbar-brand">Redux Auth App</Link>
         <ul className="nav navbar-nav">
-          <li className="nav-item">Signin</li>
+          {this.props.authenticated && Header.renderSignOut()}
+          {!this.props.authenticated && Header.renderSignIn()}
+          {!this.props.authenticated && Header.renderSignUp()}
         </ul>
       </nav>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated };
+}
+
+export default connect(mapStateToProps)(Header);
