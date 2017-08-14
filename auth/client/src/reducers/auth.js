fadefc3f367b54730d07036cc2c3b4d257017ce4
Peter Donald <peter@realityforge.org>
2017-08-14 23:48:32 +1000

    Null out error on success/failure

--- auth/client/src/reducers/auth.js
+++ auth/client/src/reducers/auth.js
@@ -3,9 +3,9 @@ import {AUTH_ERROR, AUTH_USER, UNAUTH_USER} from '../actions/types';
 export default function(state = { authenticated: false }, action) {
   switch (action.type) {
     case AUTH_USER:
-      return { ...state, authenticated: true };
+      return { ...state, authenticated: true, error: null };
     case UNAUTH_USER:
-      return { ...state, authenticated: false };
+      return { ...state, authenticated: false, error: null };
     case AUTH_ERROR:
       return { ...state, error: action.payload };
   }
