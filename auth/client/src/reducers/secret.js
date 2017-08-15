03632533e0fa5f2232aefcc0bb1c26b43400a842
Peter Donald <peter@realityforge.org>
2017-08-15 21:11:20 +1000

    Empty out state on logout

--- auth/client/src/reducers/secret.js
+++ auth/client/src/reducers/secret.js
@@ -1,9 +1,11 @@
-import {SHOW_SECRET} from '../actions/types';
+import {SHOW_SECRET, UNAUTH_USER} from '../actions/types';
 
 export default function(state = null, action) {
   switch (action.type) {
     case SHOW_SECRET:
       return action.payload;
+    case UNAUTH_USER:
+      return null;
   }
   return state;
 }
